88ad28e120cae0b5d35a9fc28943427ab9212194
dar5o <87797682+dar5o@users.noreply.github.com>
2022-02-15 11:24:12 -0800

    Update index.js

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,37 +1,62 @@
-import React from 'react';
-
+import { useState } from 'react';
 import './form.scss';
 
-class Form extends React.Component {
-
-  handleSubmit = e => {
+const Form = (props) => {
+  const [method, setMethod] = useState('');
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [showTextArea, setShowTextArea] = useState(false);
+  const [requestBody, setRequestBody] = useState('');
+  
+  const handleSubmit = e => {
     e.preventDefault();
     const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      // method:'GET',
+      // url: 'https://pokeapi.co/api/v2/pokemon',
+      method: method,
+      url: url,
     };
-    this.props.handleApiCall(formData);
+    props.handleApiCall(formData, requestBody);
+  }
+  
+ const handleMethod = e => {
+    e.preventDefault();
+    setMethod(e.target.id);
+  }
+  
+ const handleUrl = e => {
+    e.preventDefault();
+    setUrl(e.target.value);
+  }
+  
+ const handleRequestBody = e => {
+    setRequestBody(e.target.id);
+  }
+ 
+ const handleTextArea = e => {
+    setShowTextArea(!showTextArea);
+    setMethod(e.target.id)
   }
 
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-      </>
-    );
-  }
+ 
+  
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label >
+          <span>URL: </span>
+          <input onChange = {handleUrl} name='url' type='text' />
+          <button type="submit">GO!</button>
+        </label>
+        <label className="methods">
+          <span onClick={handleMethod} id="get">GET</span>
+          <span onClick={handleTextArea} id="post">POST</span>
+          <span onClick={handleTextArea} id="put">PUT</span>
+          <span onClick={handleMethod} id="delete">DELETE</span>
+        </label>
+        {showTextArea && <textarea name = "postput" rows ="10" cols="29" onChange={handleRequestBody}/>}
+      </form>
+    </>
+  );
 }
 
 export default Form;
